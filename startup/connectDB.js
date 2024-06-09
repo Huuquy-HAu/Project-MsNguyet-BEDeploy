27a5a89f3b58bea822cdef46f91f3707acb1cc86
Huuquy-HAu <quyhuu19@gmail.com>
2024-06-09 20:21:50 +0700

    fix mongodb connect

--- startup/connectDB.js
+++ startup/connectDB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 exports.connectDB = async () => {
-    await mongoose.connect('mongodb://0.0.0.0:27017/TruePhone');
+    await mongoose.connect('mongodb+srv://quyhuu19:12345sau@atlascluster.a0jn3af.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster');
     console.log('mongoDB connected');
 } 
